43983c9858a7a44e1680ce5c630508ab03dd07bb
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 11:41:14 -0300

    03-Fazendo a primeira leitura do arquivo
    Note que, da forma que está o código da App, haverá uma exceção de
    FileNotFoundException, pois o arquivo não está na raiz do projeto.
    Mova-o para a raiz do projeto e execute o programa novamente.
    
    Outra coisa: note que, nesse estado, o recurso InputStream não fechou o
    local de leitura. Devemos sempre tratar isso.

---
+++ src/app/App.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class App {
+
+	public static void main(String[] args) throws IOException{
+		// Lendo de arquivo de entrada
+		InputStream is = new FileInputStream("entrada.txt");
+		// Para trabalharmos com Streams precisamos colocar os dados num array de bytes.
+		byte[] buffer = new byte[1024];
+		// Colocar os bytes lidos do arquivo no array de bytes.
+		is.read(buffer);
+		// Constructs a new String by decoding the specified array of bytes using the platform's default charset
+		String s = new String(buffer);
+		System.out.println(s);
+		
+
+	}
+
+}
