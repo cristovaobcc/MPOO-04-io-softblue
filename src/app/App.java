5914fd2367027bdfbc34c0debc3859de5e208308
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 12:40:06 -0300

    12 - Teste de função Utils.LerArquivoComoBytes();

--- src/app/App.java
+++ src/app/App.java
@@ -1,27 +1,16 @@
 package app;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import utils.Utils;
 
 public class App {
 
 	public static void main(String[] args) throws IOException{
-		// Lendo de arquivo de entrada
-		try (InputStream is = new FileInputStream("entrada.txt") ) {
-			// Para trabalharmos com Streams precisamos colocar os dados num array de bytes.
-			byte[] buffer = new byte[4];
-			
-			// Lendo os dados de entrada em pedaços.
-			String s = "";
-			int bytesLidos;
-			while((bytesLidos = is.read(buffer)) > -1 ) {
-				s += new String(buffer, 0, bytesLidos);
-			}
-			
-			System.out.println(s);
-			System.out.println(s.length());
-		} 
+		String s = Utils.LerArquivoComoBytes("entrada.txt", 10);
+
+		System.out.println(s);
+		System.out.println(s.length());
+
 
 	}
 
