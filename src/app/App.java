43276e0b38dbd01fc7946d95550bae6be7848bcf
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 11:52:29 -0300

    05 - Fecha o recurso InputStream com o try/finally em App.java
    Note que o fluxo entrada de dados é encerrado ao fim do programa e os
    recursos que são associados a ele são liberados.
    
    Coloque um breakpoint na linha 21 e debugue o código observando o
    conteúdo do array de byte, para observar como os caracteres lidos foram
    codificados.

--- src/app/App.java
+++ src/app/App.java
@@ -7,16 +7,25 @@ import java.io.InputStream;
 public class App {
 
 	public static void main(String[] args) throws IOException{
-		// Lendo de arquivo de entrada
-		InputStream is = new FileInputStream("entrada.txt");
-		// Para trabalharmos com Streams precisamos colocar os dados num array de bytes.
-		byte[] buffer = new byte[1024];
-		// Colocar os bytes lidos do arquivo no array de bytes.
-		is.read(buffer);
-		// Constructs a new String by decoding the specified array of bytes using the platform's default charset
-		String s = new String(buffer);
-		System.out.println(s);
-		
+
+		InputStream is = null;
+
+		try {
+			// Lendo de arquivo de entrada
+			is = new FileInputStream("entrada.txt");
+			// Para trabalharmos com Streams precisamos colocar os dados num array de bytes.
+			byte[] buffer = new byte[1024];
+			// Colocar os bytes lidos do arquivo no array de bytes.
+			is.read(buffer);
+			// Constructs a new String by decoding the specified array of bytes using the platform's default charset
+			String s = new String(buffer);
+			System.out.println(s);
+		} finally {
+			if (is != null) {
+				// Closes this input stream and releases any system resources associated with the stream.
+				is.close();
+			}
+		}
 
 	}
 
