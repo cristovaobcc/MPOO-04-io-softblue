f1780fb724839fa6104767cf4ee9ba57e36a88a3
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 15:51:04 -0300

    23-Testando a aplicação com o método Utils.criaEEscreveArquivoTexto()
    Lembre de deletar o arquivo de saída antes de executar.

--- src/app/App.java
+++ src/app/App.java
@@ -12,15 +12,10 @@ public class App {
 	public static void main(String[] args) throws IOException{
 		
 		String s = "Texto para ser gravado no arquivo.";
+				
 		
-		// Faremos o caminho inverso agora: enviar dados da App para arquivos.
-		
-		try(OutputStream os = new FileOutputStream("saida.txt")){
-			// Note que, aqui, o programador não precisa montar o buffer: 
-			// o objeto String já monta ele com o método getBytes().
-			byte[] buffer = s.getBytes();
-			// Escreve os dados da string no arquivo saida.txt
-			os.write(buffer);
+		if (Utils.criaEEscreveArquivoTexto("saida.txt", s)) {
+			System.out.println("Arquivo criado com sucesso.");
 		}
 		
 		
