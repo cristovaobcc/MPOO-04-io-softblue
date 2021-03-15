2b0afe2a2778d44d712b040775ee4801d1b290fe
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 11:56:26 -0300

    06- Modificando o código para usar try-with-resources
    O efeito é o mesmo do commit anterior, a diferença é que não é preciso
    fechar manualmente o recurso InputStream is: ele é fechado
    automaticamente ao fim do código.

--- src/app/App.java
+++ src/app/App.java
@@ -7,12 +7,8 @@ import java.io.InputStream;
 public class App {
 
 	public static void main(String[] args) throws IOException{
-
-		InputStream is = null;
-
-		try {
-			// Lendo de arquivo de entrada
-			is = new FileInputStream("entrada.txt");
+		// Lendo de arquivo de entrada
+		try (InputStream is = new FileInputStream("entrada.txt") ) {
 			// Para trabalharmos com Streams precisamos colocar os dados num array de bytes.
 			byte[] buffer = new byte[1024];
 			// Colocar os bytes lidos do arquivo no array de bytes.
@@ -20,12 +16,7 @@ public class App {
 			// Constructs a new String by decoding the specified array of bytes using the platform's default charset
 			String s = new String(buffer);
 			System.out.println(s);
-		} finally {
-			if (is != null) {
-				// Closes this input stream and releases any system resources associated with the stream.
-				is.close();
-			}
-		}
+		} 
 
 	}
 
