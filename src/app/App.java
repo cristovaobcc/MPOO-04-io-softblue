824e02f6b2b872803e6b0ce7fd8e0da91a778118
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 13:40:45 -0300

    19 - Testando lerArquivoComScanner(). Falhou.
    Faltou inserir um objeto File para ser lido pelo Scanner no algortimo do
    método lerArquivoComScanner().

--- src/app/App.java
+++ src/app/App.java
@@ -11,15 +11,8 @@ public class App {
 
 	public static void main(String[] args) throws IOException{
 		
-		// Forma mais simplificada para leitura de arquivos textos.		
-		try(Scanner scanner = new Scanner(new File("entrada.txt"))){
-			String s = "";
-			while(scanner.hasNext()) {
-				String line = scanner.nextLine();
-				s += line + "\n";
-			}
-			System.out.println(s);
-		}
+		String dadosLidos = Utils.lerArquivoComScanner("entrada.txt");
+		System.out.println(dadosLidos);
 		
 	}
 
