d97623e38ea032947cb5c76e8bf2f49855fca376
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 13:21:28 -0300

    16-Testando Utils.lerArquivoComReader() na App

--- src/app/App.java
+++ src/app/App.java
@@ -4,27 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import utils.Utils;
+
 
 public class App {
 
 	public static void main(String[] args) throws IOException{
 		
-		// Classe apropriada para ler informações de texto
-		FileReader fr = new FileReader("entrada.txt");
-		
-		// Classe que lerá os dados como Strings
-		try (BufferedReader reader = new BufferedReader(fr)){
-			
-			String s = "";
-			String line;
-			while((line = reader.readLine()) != null) {
-				s += line + "\n";
-			}
-						
-			System.out.println(s);
-		}
-		
-		fr.close();
+		String dadosLidos = Utils.lerArquivoComReader("entrada.txt");
+		System.out.println(dadosLidos);
 	}
 
 }
