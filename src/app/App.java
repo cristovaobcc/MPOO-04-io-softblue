fd602732d2069f1eb53c08aa9a9d6d62e2901f1a
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-16 16:41:18 -0300

    Tirando vantagem do polimorfismo em Java com a API de I/O (parte 1)
    Execute a aplicação e veja que os dados de entrada.txt é igual aos dados
    de saida.txt.

--- src/app/App.java
+++ src/app/App.java
@@ -1,29 +1,38 @@
 package app;
 
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 
-import utils.Utils;
 
 
 public class App {
 
 	public static void main(String[] args) throws IOException{
 		
-		// Criando cópias de arquivos textos ou binários.
-		String inputFile = "entrada.txt";
-		String outputFile = "saida.txt";
+		// Polimorfismo com API de I/O
+		// Fazendo transferência de dados entre arquivos:
+		FileInputStream fin = new FileInputStream("entrada.txt");
+		FileOutputStream fout = new FileOutputStream("saida.txt");
 		
-		if (Utils.copiaArquivo(inputFile, outputFile))
-			System.out.println("Cópia realizada com sucesso.");
+		transfer(fin, fout); // usando um cast implícito para o método;
 		
+		
+	}
+	
+	// transfere dados de uma stream de entrada para uma stream de saída.
+	// Note que, com esse método não precisamos saber de onde e para onde os
+	// dados serão gravados.
+	private static void transfer(InputStream in, OutputStream out) throws IOException{
+		
+		byte[] buffer = new byte[1024]; // 1 KB
+		
+		int bytesLidos;
+		while ((bytesLidos = in.read(buffer)) > -1) {
+			out.write(buffer, 0, bytesLidos);
+		}
 	}
 
 }
