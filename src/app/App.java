bb3cbe8012c0ed719d2deac0feaa1f173e220f0a
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-15 21:31:56 -0300

    26-Teste App c/ método  Utils.criaEEscreveComPrintWriter()

--- src/app/App.java
+++ src/app/App.java
@@ -14,12 +14,16 @@ public class App {
 	public static void main(String[] args) throws IOException{
 		
 		String s1 = "Texto para ser gravado no arquivo!";
+		String s2 = "Novo texto para ser gravado.";
 		
-		// Usando outra forma de se escrever no arquivo:
-		try(PrintWriter pw = new PrintWriter("saida.txt")){
-			// Com PrintWriter você tem a opção de dar saída formatada de arquivos.
-			pw.println(s1); // note que a saída será impressa com quebra de linha. Vide arquivo gerado!
+		ArrayList<String> dados = new ArrayList<String>();
+		dados.add(s1);
+		dados.add(s2);
+		
+		if (Utils.criaEEscreveComPrintWriter("saida.txt", dados)) {
+			System.out.println("Arquivo criado com exito!");
 		}
+		
 			
 		
 	}
